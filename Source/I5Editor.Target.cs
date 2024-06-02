7f85b7f7d072eb83a7b2bd398f61c89087650d35
HerogonStudios <85625864+HerogonStudios@users.noreply.github.com>
2024-06-02 19:02:52 -0400

    Update I5Editor.Target.cs

--- Source/I5Editor.Target.cs
+++ Source/I5Editor.Target.cs
@@ -8,7 +8,7 @@ public class I5EditorTarget : TargetRules
 	public I5EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "I5" } );
 	}
